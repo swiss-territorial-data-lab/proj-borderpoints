0d6f7edcaf77fa475e5c4f008dff175b8f24e903
GwenaelleSa <gwenaelle.salamin@gmail.com>
2024-04-03 12:40:42 +0200

    Write script to convert images

---
+++ scripts/functions/fct_misc.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from loguru import logger
+
+
+def format_logger(logger):
+    """Format the logger from loguru
+
+    Args:
+        logger: logger object from loguru
+
+    Returns:
+        logger: formatted logger object
+    """
+
+    logger.remove()
+    logger.add(sys.stderr, format="{time:YYYY-MM-DD HH:mm:ss} - {level} - {message}",
+            level="INFO", filter=lambda record: record["level"].no < 25)
+    logger.add(sys.stderr, format="{time:YYYY-MM-DD HH:mm:ss} - <green>{level}</green> - {message}",
+            level="SUCCESS", filter=lambda record: record["level"].no < 30)
+    logger.add(sys.stderr, format="{time:YYYY-MM-DD HH:mm:ss} - <yellow>{level}</yellow> - {message}",
+            level="WARNING", filter=lambda record: record["level"].no < 40)
+    logger.add(sys.stderr, format="{time:YYYY-MM-DD HH:mm:ss} - <red>{level}</red> - <level>{message}</level>",
+            level="ERROR")
+
+    return logger
