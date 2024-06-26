d672118c54e02a13139f8e423244935685f24d45
GwenaelleSa <gwenaelle.salamin@gmail.com>
2024-06-26 09:34:26 +0200

    Make boxplot of statistics

--- scripts/symbol_recognition/feature_engineering.py
+++ scripts/symbol_recognition/feature_engineering.py
@@ -9,6 +9,7 @@ from yaml import load, FullLoader
 import geopandas as gpd
 import pandas as pd
 import rasterio as rio
+import rasterstats as rst
 from rasterio.mask import mask
 
 from matplotlib import pyplot as plt
@@ -44,6 +45,9 @@ TILE_DIR = cfg['tile_dir']
 IMAGE_DELIMITATION = cfg['image_delimitation']
 TILES = cfg['tiles']
 
+BAND_CORRESPONDENCE = {0: 'R', 1: 'G', 2: 'B'}
+STAT_LIST = ['min', 'std', 'mean', 'median']
+
 os.chdir(WORKING_DIR)
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
@@ -62,19 +66,37 @@ pxl_values_dict = {
     1: {cat: [] for cat in smbl_delimitation_gdf.CATEGORY.unique()}, 
     2: {cat: [] for cat in smbl_delimitation_gdf.CATEGORY.unique()}
 }
+stats_df_dict = {band: pd.DataFrame() for band in BAND_CORRESPONDENCE.keys()}
 for tile in tqdm(smbl_delimitation_gdf.name.unique(), desc="Extract pixel values from tiles"):
     symbols_on_tile = smbl_delimitation_gdf[smbl_delimitation_gdf.name==tile].copy()
     
     with rio.open(os.path.join(TILE_DIR, tile + '.tif')) as src:
         for category in symbols_on_tile.CATEGORY.unique():
             tmp_pxl_values, _ = mask(src, symbols_on_tile.loc[symbols_on_tile.CATEGORY == category, 'geometry'], crop=True, filled=False)
-            for band in range(3):
+            for band in BAND_CORRESPONDENCE.keys():
                 pxl_values_dict[band][category].extend(tmp_pxl_values[band].data[tmp_pxl_values[band].mask].tolist())
+
+    for band in BAND_CORRESPONDENCE.keys():
+        tmp_stats_list = rst.zonal_stats(
+            symbols_on_tile, os.path.join(TILE_DIR, tile + '.tif'), stats=STAT_LIST, band_num=band
+        )
+        tmp_stats_df = pd.DataFrame.from_records(tmp_stats_list)
+        tmp_stats_df['CATEGORY'] = symbols_on_tile.CATEGORY.tolist()
+    
+        stats_df_dict[band] = pd.concat([stats_df_dict[band], tmp_stats_df], ignore_index=True)
                 
-for band in tqdm(range(3), desc='Produce boxplots for each band'):
+for band in tqdm(BAND_CORRESPONDENCE.keys(), desc='Produce boxplots for each band'):
     labels, data = [*zip(*pxl_values_dict[band].items())]
 
     plt.boxplot(data)
     plt.xticks(range(1, len(labels) + 1), labels)
-    plt.savefig(os.path.join(OUTPUT_DIR, f'boxplot_pixels_{band}.png'))
+    plt.title(f'Pixel values on the {BAND_CORRESPONDENCE[band]} band')
+    plt.savefig(os.path.join(OUTPUT_DIR, f'boxplot_pixels_{BAND_CORRESPONDENCE[band]}.png'), bbox_inches='tight')
     plt.close()
+
+    for stat in STAT_LIST:
+        stats_df = stats_df_dict[band].loc[: , ['CATEGORY', stat]].copy()
+        stats_df.plot.box(by='CATEGORY')
+        plt.title(f'{stat.title()} on the {BAND_CORRESPONDENCE[band]} band')
+        plt.savefig(os.path.join(OUTPUT_DIR, f'boxplot_stats_{BAND_CORRESPONDENCE[band]}_{stat}.png'), bbox_inches='tight')
+        plt.close()
